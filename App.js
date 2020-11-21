b5f88e6277c500acd68caf674177b26621fb1b56
Nao <nmatsuo.63@gmail.com>
2020-11-21 16:08:22 +0900

    sec4finished

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +9,55 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    // backgroundColor: 'red',
+    width: 100,
+  },
+  rightContainer: {
+    // backgroundColor: 'blue',
+    flex: 1,
+    flexDirection: 'column',
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{
+              // uri: 'https://reactnative.dev/img/tiny_logo.png',
+              uri: 'https://picsum.photos/id/10/200/300/',
+            }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3} styel={styles.text}>
+            Used to truncate the text with an ellipsis after computing the text
+            layout, including line wrapping, such that the total number of lines
+            does not exceed this number.
+          </Text>
+          <Text style={styles.subText}>ReactNews</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
